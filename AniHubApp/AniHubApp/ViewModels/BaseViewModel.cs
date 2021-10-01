ce0165cff9be8f47363438d8bdf44050d3d0e6f1
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-01 17:41:20 +0000

    Add required ViewModels for app

---
+++ AniHubApp/AniHubApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AniHubApp.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        protected INavigationService NavigationService { get; }
+
+        public BaseViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
