02e7f775483047f4303c80cd59b8f483a922db2c
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-01 20:54:47 -0400

    Create NavigationConstant

---
+++ AniHubApp/AniHubApp/NavigationConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AniHubApp
+{
+    public static class NavigationConstants
+    {
+        public static class Paths
+        {
+            public const string Main = "Main";
+            public const string Home = "Home";
+            public const string Search = "Search";
+            public const string Favorites = "Favorites";
+            public const string AnimeDetail = "AnimeDetail";
+            public const string AnimeSongs = "AnimeSongs";
+        }
+
+        public static class Parameters
+        {
+            public const string AnimeId = "AnimeId";
+            public const string AnimeSongId = "AnimeSongId";
+        }
+    }
+}
