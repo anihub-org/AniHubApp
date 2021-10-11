dd8f3d34706829c609e33ef12090bd98e3145cba
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-10 21:04:52 -0400

    Implement functionality for HomeViewModel

---
+++ AniHubApp/AniHubApp/Models/Season.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AniHubApp.Models
+{
+    public class Season
+    {
+        private enum Seasons
+        {
+            Winter,
+            Spring,
+            Summer,
+            Fall
+        }
+
+        public static int GetCurrentSeasonValue()
+        {
+            var currentDate = DateTime.Now;
+            var leapDayCount = Convert.ToInt32(DateTime.IsLeapYear(currentDate.Year) && currentDate.DayOfYear > 59);
+
+            int day = currentDate.DayOfYear - leapDayCount;
+
+            if (day < 80 || day >= 355)
+            {
+                return (int)Seasons.Winter;
+            }
+
+            if (day >= 80 && day < 172)
+            {
+                return (int)Seasons.Spring;
+            }
+
+            if (day >= 172 && day < 266)
+            {
+                return (int)Seasons.Summer;
+            }
+
+            return (int)Seasons.Fall;
+        }
+    }
+}
--- AniHubApp/AniHubApp/ViewModels/HomeViewModel.cs
+++ AniHubApp/AniHubApp/ViewModels/HomeViewModel.cs
@@ -5,14 +5,79 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Essentials;
+using System.Linq;
+using Prism.Services;
 
 namespace AniHubApp.ViewModels
 {
-    class HomeViewModel : BaseViewModel
+    public class HomeViewModel : BaseViewModel, IInitialize
     {
-        public HomeViewModel(INavigationService navigationService, IAniApiService aniApiService) : base(navigationService, aniApiService)
-        {
+        public const int BASE_POPULAR_ANIME_SCORE = 80;
 
+        public ObservableCollection<Anime> PopularAnimes { get; set; }
+        public ObservableCollection<Anime> SeasonalAnimes { get; set; }
+        public ObservableCollection<Song> SongSuggestions { get; set; }
+
+        private IPageDialogService _pageDialogService;
+
+        public HomeViewModel(INavigationService navigationService, IAniApiService aniApiService, IPageDialogService pageDialogService) : base(navigationService, aniApiService)
+        {
+            _pageDialogService = pageDialogService;
+        }
+
+        public void Initialize(INavigationParameters parameters)
+        {
+            GetSeasonalAnimes();
+            GetPopularAnimes();
+            GetSongSuggestions();
+        }
+
+        private async void GetPopularAnimes()
+        {
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                ShowNetworkConnectionError();
+                return;
+            }
+
+            var response = await AniApiService.GetAnimesAsync();
+
+            var popularAnimes = response.Data.Animes.Where(anime => anime.Score >= BASE_POPULAR_ANIME_SCORE);
+            PopularAnimes = new ObservableCollection<Anime>(popularAnimes);
+        }
+
+        private async void GetSeasonalAnimes()
+        {
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                ShowNetworkConnectionError();
+                return;
+            }
+
+            var queryParams = new AnimeListQueryParams(Season.GetCurrentSeasonValue());
+
+            var response = await AniApiService.GetAnimesAsync(queryParams);
+            SeasonalAnimes = new ObservableCollection<Anime>(response.Data.Animes);
+        }
+
+        private async void GetSongSuggestions()
+        {
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                ShowNetworkConnectionError();
+                return;
+            }
+
+            var queryParams = new AnimeSongsListQueryParams(Season.GetCurrentSeasonValue());
+
+            var response = await AniApiService.GetAnimeSongsAsync(queryParams);
+            SongSuggestions = new ObservableCollection<Song>(response.Data.Songs);
+        }
+
+        private async void ShowNetworkConnectionError()
+        {
+            await _pageDialogService.DisplayAlertAsync("Error", "Missing network connection. Try again later", "Ok");
         }
     }
 }
