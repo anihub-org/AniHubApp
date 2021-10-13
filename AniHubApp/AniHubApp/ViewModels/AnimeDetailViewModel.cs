d3a02a45ac845d17edc0353998e1d6dc192cf730
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-13 01:17:59 -0400

    Implement AnimeDetailViewModel

--- AniHubApp/AniHubApp/ViewModels/AnimeDetailViewModel.cs
+++ AniHubApp/AniHubApp/ViewModels/AnimeDetailViewModel.cs
@@ -1,16 +1,40 @@
-﻿using AniHubApp.Services;
+﻿using AniHubApp.Models;
+using AniHubApp.Services;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace AniHubApp.ViewModels
 {
-    public class AnimeDetailViewModel : BaseViewModel
+    public class AnimeDetailViewModel : BaseViewModel, INavigatedAware
     {
-        public AnimeDetailViewModel(INavigationService navigationService, IAniApiService aniApiService) : base(navigationService)
-        {
+        public Anime Anime { get; set; }
 
+        public ICommand NavigateToSongListCommand { get; set; }
+
+        public AnimeDetailViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            NavigateToSongListCommand = new Command(OnNavigateToAnimeSongs);
+        }
+
+        public async void OnNavigateToAnimeSongs()
+        {
+            var parameters = new NavigationParameters
+            {
+                {"anime_id", Anime.AnimeID }
+            };
+
+            await NavigationService.NavigateAsync($"{NavigationConstants.Paths.AnimeSongListPage}", parameters);
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters) { }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            Anime = parameters.GetValue<Anime>("anime");
         }
     }
 }
