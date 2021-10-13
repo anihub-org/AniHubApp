db514707520ae93071260e62b51a5a8003493b6d
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-13 03:06:54 -0400

    Implemente Search View Model

--- AniHubApp/AniHubApp/ViewModels/SearchViewModel.cs
+++ AniHubApp/AniHubApp/ViewModels/SearchViewModel.cs
@@ -1,16 +1,58 @@
-﻿using AniHubApp.Services;
+﻿using AniHubApp.Models;
+using AniHubApp.Services;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
+using Xamarin.Essentials;
 
 namespace AniHubApp.ViewModels
 {
-    class SearchViewModel : BaseViewModel
+    public class SearchViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public SearchViewModel(INavigationService navigationService) : base(navigationService)
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<AnimeGenre> AnimeGenres { get; set; } = new ObservableCollection<AnimeGenre>()
         {
+            new AnimeGenre("Action", "FullMetalAlchemistBrotherhood.jpg"),
+            new AnimeGenre("Adventure", "OnePiece.jpg"),
+            new AnimeGenre("Comedy", "Saiki.jpg"),
+            new AnimeGenre("Fantasy", "TheSevenDeadlySins.jpg"),
+            new AnimeGenre("Horror", "TokyoGhoul.jpg"),
+            new AnimeGenre("Mecha", "CodeGeass.jpg"),
+            new AnimeGenre("Music", "YourLieInApril.jpg"),
+            new AnimeGenre("Slice Of Life", "Anohana.jpg"),
+            new AnimeGenre("Sports", "Haikyu.jpg"),
+            new AnimeGenre("Romance", "FruitsBasket.jpg")
+        };
 
+        private IAniApiService _aniApiService;
+        private IPageDialogService _pageDialogService;
+        public SearchViewModel(INavigationService navigationService, IAniApiService aniApiService, IPageDialogService pageDialogService) : base(navigationService)
+        {
+            _pageDialogService = pageDialogService;
+            _aniApiService = aniApiService;
+        }
+
+        //public void Initialize(INavigationParameters parameters)
+        //{
+        //    AnimeGenres.Add("Action");
+        //    AnimeGenres.Add("Adventure");
+        //    AnimeGenres.Add("Comedy");
+        //    AnimeGenres.Add("Fantasy");
+        //    AnimeGenres.Add("Horror");
+        //    AnimeGenres.Add("Mecha");
+        //    AnimeGenres.Add("Music");
+        //    AnimeGenres.Add("Slice Of Life");
+        //    AnimeGenres.Add("Sports");
+        //    AnimeGenres.Add("Romance");
+        //}
+
+        private async void ShowNetworkConnectionError()
+        {
+            await _pageDialogService.DisplayAlertAsync("Error", "Missing network connection. Try again later", "Ok");
         }
     }
 }
