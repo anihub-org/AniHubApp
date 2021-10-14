2c0798607891c08ca3f7c807d916c5e6b056b0aa
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-14 01:01:53 -0400

    Implement TimeFormatConverter to convert song duration in milliseconds to minutes

---
+++ AniHubApp/AniHubApp/Converters/TimeFormatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AniHubApp.Converters
+{
+    public class TimeFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var MillisecondsDuration = System.Convert.ToDouble(value);
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(MillisecondsDuration);
+
+            return string.Format("{0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
