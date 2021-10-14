f76741cda783f52de51058dfc75a5055fcf72592
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-14 02:10:20 -0400

    Define and implement cache service

---
+++ AniHubApp/AniHubApp/Services/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AniHubApp.Services
+{
+    public interface ICacheService
+    {
+        void Add<T>(string key, T data, TimeSpan expiresIn);
+        T Get<T>(string key);
+        bool IsExpired(string key);
+        bool Exists(string key);
+        bool IsKeyValid(string key);
+    }
+}
