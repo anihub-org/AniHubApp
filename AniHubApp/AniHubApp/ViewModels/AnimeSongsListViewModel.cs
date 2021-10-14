706f68e9fa8c58025529b49c7c367282586a1f4a
aledeltoro <alejandroencarnacion1@gmail.com>
2021-10-13 20:17:28 -0400

    Implement AnimeSongsListViewModel

--- AniHubApp/AniHubApp/ViewModels/AnimeSongsListViewModel.cs
+++ AniHubApp/AniHubApp/ViewModels/AnimeSongsListViewModel.cs
@@ -3,14 +3,60 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using AniHubApp.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Prism.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace AniHubApp.ViewModels
 {
-    public class AnimeSongsListViewModel: BaseViewModel
+    public class AnimeSongsListViewModel: BaseViewModel, INavigatedAware
     {
-        public AnimeSongsListViewModel(INavigationService navigationService) : base(navigationService)
-        {
+        public ObservableCollection<Song> AnimeSongs { get; set; }
 
+        private IAniApiService _aniApiService;
+        private IPageDialogService _pageDialogService;
+
+        public ICommand RedirectToSpotifyCommand { get; set; }
+
+        public AnimeSongsListViewModel(INavigationService navigationService, IPageDialogService pageDialogService, IAniApiService aniApiService) : base(navigationService)
+        {
+            _aniApiService = aniApiService;
+            _pageDialogService = pageDialogService;
+
+            RedirectToSpotifyCommand = new Command<string>(OnRedirectToSpotify);
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters) { }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            var animeId = parameters.GetValue<string>("anime_id");
+
+            GetSongsByAnimeID(animeId);
+        }
+
+        private async void GetSongsByAnimeID(string animeId)
+        {
+            var queryParams = new AnimeSongsListQueryParams(animeId: animeId);
+
+            var response = await _aniApiService.GetAnimeSongsAsync(queryParams);
+
+            AnimeSongs = new ObservableCollection<Song>(response.Data.Songs);
+        }
+
+        private async void OnRedirectToSpotify(string spotifyURL)
+        {
+            var spotifyURI = new Uri(spotifyURL);
+
+            bool canRedirect = await _pageDialogService.DisplayAlertAsync("Info", "Are you sure you want to redirect to Spotify?", "Yes", "No");
+
+            if (canRedirect)
+            {
+                await Browser.OpenAsync(spotifyURI, BrowserLaunchMode.SystemPreferred);
+            }
         }
     }
 }
