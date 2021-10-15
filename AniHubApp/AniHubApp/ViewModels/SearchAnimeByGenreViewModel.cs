d0473a60007d78c24c47c5d2ec9a9bc396029a5d
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-15 00:56:46 -0400

    Add SelectedItem on collectionview

--- AniHubApp/AniHubApp/ViewModels/SearchAnimeByGenreViewModel.cs
+++ AniHubApp/AniHubApp/ViewModels/SearchAnimeByGenreViewModel.cs
@@ -7,6 +7,8 @@ using AniHubApp.Models;
 using AniHubApp.Services;
 using Prism.Navigation;
 using Prism.Services;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace AniHubApp.ViewModels
 {
@@ -15,23 +17,54 @@ namespace AniHubApp.ViewModels
         public ObservableCollection<Anime> AnimesByGenre { get; set; }
         public AnimeGenre AnimeSpecificGenre { get; set; }
 
+        private Anime _selectedAnime;
+        public Anime SelectAnime
+        {
+            get
+            {
+                return _selectedAnime;
+            }
+            set
+            {
+                _selectedAnime = value;
+                if (_selectedAnime != null)
+                {
+                    SelectedAnimeCommand.Execute(_selectedAnime);
+                }
+            }
+        }
+
+        public ICommand SelectedAnimeCommand { get; }
         private IAniApiService _aniApiService;
         private IPageDialogService _pageDialogService;
         public SearchAnimeByGenreViewModel(INavigationService navigationService, IAniApiService aniApiService, IPageDialogService pageDialogService) : base(navigationService)
         {
             _pageDialogService = pageDialogService;
             _aniApiService = aniApiService;
+            SelectedAnimeCommand = new Command<Anime>(OnSelectedAnime);
         }
+
+        private async void OnSelectedAnime(Anime anime)
+        {
+            var parameters = new NavigationParameters
+            {
+                {"anime", anime }
+            };
+
+            await NavigationService.NavigateAsync($"{NavigationConstants.Paths.AnimeDetailPage}", parameters);
+        }
+
+        public async void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
         public async void OnNavigatedTo(INavigationParameters parameters)
         {
             AnimeSpecificGenre = parameters.GetValue<AnimeGenre>("genre");
             var queryParams = new AnimeListQueryParams(genre: AnimeSpecificGenre.Name);
             var response = await _aniApiService.GetAnimesAsync(queryParams);
             AnimesByGenre = new ObservableCollection<Anime>(response.Data.Animes);
-        }
-
-        public void OnNavigatedFrom(INavigationParameters parameters)
-        {
         }
     }
 }
--- AniHubApp/AniHubApp/ViewModels/SearchAnimeByNameViewModel.cs
+++ AniHubApp/AniHubApp/ViewModels/SearchAnimeByNameViewModel.cs
@@ -15,6 +15,23 @@ namespace AniHubApp.ViewModels
     public class SearchAnimeByNameViewModel : BaseViewModel
     {
         public ObservableCollection<Anime> AnimesByName { get; set; }
+        private Anime _selectedAnime;
+        public Anime SelectAnime
+        {
+            get
+            {
+                return _selectedAnime;
+            }
+            set
+            {
+                _selectedAnime = value;
+                if (_selectedAnime != null)
+                {
+                    SelectedAnimeCommand.Execute(_selectedAnime);
+                }
+            }
+        }
+        public ICommand SelectedAnimeCommand { get; }
         public ICommand SearchCommand { get;  }
 
         private IAniApiService _aniApiService;
@@ -24,6 +41,17 @@ namespace AniHubApp.ViewModels
             _pageDialogService = pageDialogService;
             _aniApiService = aniApiService;
             SearchCommand = new Command<string>(OnSearchAnimeByName);
+            SelectedAnimeCommand = new Command<Anime>(OnSelectedAnime);
+
+        }
+        private async void OnSelectedAnime(Anime anime)
+        {
+            var parameters = new NavigationParameters
+            {
+                {"anime", anime }
+            };
+
+            await NavigationService.NavigateAsync($"{NavigationConstants.Paths.AnimeDetailPage}", parameters);
         }
 
         private async void OnSearchAnimeByName(string animeTitle)
