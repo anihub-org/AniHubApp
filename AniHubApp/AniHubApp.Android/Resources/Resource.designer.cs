4fa9b8f1599a9a8cddacf56ca968dab446d794e6
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-16 03:37:24 -0400

    Add Empty View to Favorites Page

--- AniHubApp/AniHubApp.Android/Resources/Resource.designer.cs
+++ AniHubApp/AniHubApp.Android/Resources/Resource.designer.cs
@@ -15443,34 +15443,37 @@ namespace AniHubApp.Droid
 			public const int OnePiece = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int Saiki = 2131165332;
+			public const int onePunchMan = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int searchIcon24dp = 2131165333;
+			public const int Saiki = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int searchIcon48dp = 2131165334;
+			public const int searchIcon24dp = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int searchIconSmall = 2131165335;
+			public const int searchIcon48dp = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int test_custom_background = 2131165336;
+			public const int searchIconSmall = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int TheSevenDeadlySins = 2131165337;
+			public const int test_custom_background = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int TokyoGhoul = 2131165338;
+			public const int TheSevenDeadlySins = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_dark = 2131165339;
+			public const int TokyoGhoul = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_light = 2131165340;
+			public const int tooltip_frame_dark = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int YourLieInApril = 2131165341;
+			public const int tooltip_frame_light = 2131165341;
+			
+			// aapt resource value: 0x7F07009E
+			public const int YourLieInApril = 2131165342;
 			
 			static Drawable()
 			{
